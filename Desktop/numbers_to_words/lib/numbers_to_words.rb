8e91f041c1fb5ca2dd32117befe063e0fbe58cbc
Daniel Burbach and Quinn Miller <quinn@>
2018-11-14 11:23:47 -0800

    wrote our method to be tested

---
+++ Desktop/numbers_to_words/lib/numbers_to_words.rb
@@ -0,0 +1,8 @@
+class Integer
+  def convert_numbers
+    new_array = []
+    if self == 1
+      new_array.push("one")
+    end
+  end
+end
--- Desktop/numbers_to_words/spec/numbers_to_words_spec.rb
+++ Desktop/numbers_to_words/spec/numbers_to_words_spec.rb
@@ -4,6 +4,6 @@ require('numbers_to_words')
 
 describe('#convert_numbers') do
   it("returns a written number to a numerical entry") do
-    expect("1".convert_numbers()).to(eq("one"))
+    expect(1.convert_numbers()).to(eq("one"))
   end
 end
