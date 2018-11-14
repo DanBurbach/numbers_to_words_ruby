e210ed449a1910a124bddc337211b5fcec30e6e2
Daniel Burbach and Quinn Miller <quinn@>
2018-11-14 11:45:32 -0800

    pre lunch commit

--- Desktop/numbers_to_words/lib/numbers_to_words.rb
+++ Desktop/numbers_to_words/lib/numbers_to_words.rb
@@ -1,9 +1,12 @@
 class Integer
   def convert_numbers
-    new_array = []
-    if self == 1
-      new_array.push("one")
-    end
+    number = self.to_s
+    new_array = number.split(//)
+    if new_array.length >
+    binding.pry
+    #if self == 1
+    #  new_array.push("one")
+    #end
     return new_array*' '
   end
 end
--- Desktop/numbers_to_words/spec/numbers_to_words_spec.rb
+++ Desktop/numbers_to_words/spec/numbers_to_words_spec.rb
@@ -1,9 +1,13 @@
 require('rspec')
 require('numbers_to_words')
+require('pry')
 
 
 describe('#convert_numbers') do
-  it("returns a written number to a numerical entry") do
-    expect(1.convert_numbers()).to(eq("one"))
+  #it("returns a written number to a numerical entry") do
+  #  expect(1.convert_numbers()).to(eq("one"))
+  #end
+  it("evaluates the length of the number up to one trillion") do
+    expect(1000000000000.convert_numbers()).to(eq("one"))
   end
 end
